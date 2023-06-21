5de9948c9a3c54a04adad874d23639244831ab1f
pavva91 <valerio.mattioli@protonmail.com>
2023-06-21 17:09:55 +0200

    initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.print("James Bond");
+	}
+
+	@GetMapping
+	public Customer getCustomer(){
+		return new Customer(1L, "James Bond");
+	}
+
+	class Customer {
+		private Long id;
+		private String name;
+
+		public Customer(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+	}
+}
