9eb0e6ada9ce3cc27918fcfbcd13a5f8a364dac8
pavva91 <valerio.mattioli@proton.me>
2023-11-30 18:00:16 +0100

    Start N-Tier Architecture

---
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -0,0 +1,25 @@
+package com.example.demo.customer;
+
+public class Customer {
+	private Long id;
+	private String name;
+
+	public Customer(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer{" + "id=" + id + ", name='" + name + '\'' + "}";
+	}
+
+}
---
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -0,0 +1,13 @@
+package com.example.demo.customer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+	@GetMapping("/")
+	public Customer getCustomer() {
+		return new Customer(1L, "James Bond");
+	}
+}
